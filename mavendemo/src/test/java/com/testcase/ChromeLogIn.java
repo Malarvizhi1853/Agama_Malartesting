d48f7c4fe449190ce95c72480f6017dd957e3866
Kranthi Kiriti Mekala <kranthi.mekala@outlook.com>
2020-06-04 16:17:33 -0700

    commit the changes

--- mavendemo/src/test/java/com/testcase/ChromeLogIn.java
+++ mavendemo/src/test/java/com/testcase/ChromeLogIn.java
@@ -11,13 +11,12 @@ public class ChromeLogIn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				
-
    //System.setProperty("webdriver.chrome.driver", "C:/Users/krant/Downloads/Selenium Java/chromedriver_win32/chromedriver.exe");
 		
-	
+
 	WebDriverManager.chromedriver().setup();
 	WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com/");
+	driver.get("https://www.google.com/");
 		
 		System.out.println("Title of the page is  "+ driver.getTitle());
 		//driver.close();
