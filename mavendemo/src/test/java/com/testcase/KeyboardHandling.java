8e0e16ba582a866029ded64bd51be4236d6bf7dc
Malarvizhi1853 <malarinbox@gmail.com>
2020-06-08 12:27:28 -0700

    testng issue fixed

---
+++ mavendemo/src/test/java/com/testcase/KeyboardHandling.java
@@ -0,0 +1,27 @@
+package com.testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class KeyboardHandling {
+
+	@Test
+	public void login() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	driver.findElement(By.id("email")).sendKeys("abcd@gmail.com");
+	Actions action=new Actions(driver);
+	action.sendKeys(Keys.TAB).perform();
+	action.sendKeys("abcd").perform();
+	action.sendKeys(Keys.ENTER).perform();
+	
+}
+}
